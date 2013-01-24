2888bf32cc7b05837b1662981250a40f342bc319
christian-bromann <contact@christian-bromann.com>
2013-01-25 00:50:09 +0100

    refactored folder structure

--- main.js
+++ main.js
@@ -38,26 +38,3 @@
 
 
 }());
-
-// requestAnimationFrame shim
-(function() {
-    var i = 0,
-        lastTime = 0,
-        vendors = ['ms', 'moz', 'webkit', 'o'];
-    
-    while (i < vendors.length && !window.requestAnimationFrame) {
-        window.requestAnimationFrame = window[vendors[i] + 'RequestAnimationFrame'];
-        i++;
-    }
-    
-    if (!window.requestAnimationFrame) {
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime(),
-                timeToCall = Math.max(0, 1000 / 60 - currTime + lastTime),
-                id = setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
-            
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-    }
-}());
