3b6835976e36f4801b2aca204265461fb7da20a0
christian-bromann <contact@christian-bromann.com>
2013-10-17 18:02:02 +0200

    saved changes done before/after last exhibition

--- blog/getImages.php
+++ blog/getImages.php
@@ -1,27 +1,33 @@
 <?php
+
+class Image {
+    public function __construct($path) {
+        $this->path     = $path;
+        $this->geoData = file_get_contents('img/uploads/'.substr($path,0,count($path)-5).'.txt');
+    }
+}
+
 $loadedImages = preg_split('/,/',$_POST['loaded']);
 $ignoredFiles = array('.','..','.DS_Store');
-$newImages    = array();
-$geoData      = array();
+$images       = array();
 
 if (isset($loadedImages) && $handle = opendir('img/uploads')) {
-    
+
     while (false !== ($entry = readdir($handle))) {
         
         if(in_array($entry,$ignoredFiles) || in_array($entry,$loadedImages) || substr($entry,count($entry)-5) === '.txt') {
             continue;
         }
 
-        array_push($geoData, file_get_contents('img/uploads/'.substr($entry,0,count($entry)-5).'.txt'));
-        array_push($newImages, $entry);
+        $date = substr($entry,0,count($entry)-5);
+        $images[$date] = new Image($entry);
     }
 
     header('Cache-Control: no-cache, must-revalidate');
     header('Content-type: application/json');
 
-    if(count($newImages)) {
-        echo '{"images": ["'.join('","',$newImages).'"],';
-        echo ' "geoData":["'.join('","',str_replace("\"","'",str_replace("\\", "", $geoData))).'"]}';
+    if(count($images) && asort($images)) {
+        echo json_encode($images);
     } else {
         echo '{}';
     }
--- blog/js/main.js
+++ blog/js/main.js
@@ -3,71 +3,114 @@ var spraycanApp = function(window,document,$,undefined) {
     // go into strict mode
     "use strict";
 
+    Object.prototype.keys = function(obj){
+        var keys = [];
+        for(var key in obj){
+            keys.push(key);
+        }
+        return keys;
+    };
+
     var section = $('section'),
         loadedImages = [],
-        firstRequest = true,
+        imagesToShow = [],
         img,article,geoData,i,loaded,adressbar;
 
     function getImages() {
-        $.ajax('/getImages.php', {
+        $.ajax('getImages.php', {
             type: 'POST',
             data: 'loaded='+loadedImages.join(','),
             success: function(data) {
 
-                if(data.images && data.images.length) {
-                    i = 0;
+                if(!$.isEmptyObject(data)) {
                     loaded = 0;
-                    data.images.forEach(function(image) {
+                    var keyDate,geoData;
+                    var i = 0;
 
-                        loadedImages.push(image);
+                    for(keyDate in data){
 
-                        try {
-                            geoData = JSON.parse(data.geoData[i].replace(/'/g,'"').replace(/\//,''));
-                        } catch(e) {
-                            geoData = {formatted_address:'Adresse nicht bekannt'};
+                        var image = data[keyDate];
+                        loadedImages.push(image.path);
+
+                        if(i < Object.keys(data).length - 15) {
+                            imagesToShow.push(image);
+                            ++i;
+                            continue;
                         }
-                        adressbar = $('<div />').html(geoData.formatted_address);
 
-                        article = $('<a />')
-                            .css('display','none')
-                            .attr('href','/img/uploads/'+image)
-                            .attr('rel','lightbox')
-                            .attr('title',geoData.formatted_address);
-                        img = $('<img />').attr('src','/img/uploads/'+image);
-
-                        var objects = {article:article,adressbar:adressbar,img:img};
-
-                        img.load(function() {
-                            objects.article.append(objects.img);
-                            objects.article.append(objects.adressbar);
-                            section.prepend(objects.article);
-                            objects.article.fadeIn();
-                            objects.article.colorbox({maxWidth: '80%'});
-                            ++loaded;
-
-                            if(loaded === data.images.length) {
-                                firstRequest = false;
-                                getImages();
-                            }
-                        });
-
+                        displayImage(image,true);
                         ++i;
-                    });
+                    }
+                } else {
+                    window.setTimeout(getImages,1000);
                 }
             },
             error: function(e) {
                 console.error(e);
             }
         });
+    }
 
-        if(!firstRequest) {
-            window.setTimeout(getImages,1000);
+    function displayImage(image,prepend) {
+        var geoData;
+
+        try {
+            geoData = JSON.parse(image.geoData.replace(/\\/g,''));
+        } catch(e) {
+            geoData = {formatted_address:'Adresse nicht bekannt'};
         }
+        var adressbar = $('<div />').html(geoData.formatted_address);
+
+        var article = $('<a />')
+            .css('display','none')
+            .attr('href','/img/uploads/'+image.path)
+            .attr('rel','lightbox')
+            .attr('title',geoData.formatted_address);
+
+        var img     = $('<img />').attr('src','/img/uploads/'+image.path);
+        var objects = {article:article,adressbar:adressbar,img:img};
+
+        (function(img,obj) {
+            var objects = obj,
+                image   = img;
+
+            image.load(function() {
+                objects.article.append(objects.img);
+                objects.article.append(objects.adressbar);
+
+                if(prepend) {
+                    section.prepend(objects.article);
+                } else {
+                    section.append(objects.article);
+                }
+
+                objects.article.fadeIn();
+                objects.article.colorbox({maxWidth: '80%'});
+                ++loaded;
+
+                if(loaded === 10) {//Object.keys(data).length
+                    getImages();
+                }
+            });
+        })(img,objects);
     }
 
     // on document load
     (function() {
         getImages();
+
+        var $window = $(window);
+        $window.scroll(function() {
+
+            if($window.scrollTop() == $(document).height() - $window.height()) {
+
+                for(var i = 0; i < 6; ++i) {
+                    var image = imagesToShow.pop();
+                    if(image) displayImage(image,false);
+                }
+
+            }
+        });
     })();
 
     // public vars and functions
